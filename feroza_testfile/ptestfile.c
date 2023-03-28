96c63c7c82f513b5ce77cd86383823c7a31fd92d
GArdennes <thekevin.afachao@gmail.com>
2023-03-28 13:24:55 -0700

    Trial

--- feroza_testfile/ptestfile.c
+++ feroza_testfile/ptestfile.c
@@ -1,4 +1,65 @@
 #include "maintest.h"
+/**
+* print_char - prints a single character
+* @args: va_list of arguments
+* @buffer: buffer to write to
+* @chars_written: pointer to number of characters written
+*/
+void print_char(va_list args, char *buffer, int *chars_written)
+{
+char c = va_arg(args, int);
+buffer[(*chars_written)++] = c;
+}
+/**
+* print_string - prints a string
+* @args: va_list of arguments
+* @buffer: buffer to write to
+* @chars_written: pointer to number of characters written
+*/
+void print_string(va_list args, char *buffer, int *chars_written)
+{
+char *s = va_arg(args, char *);
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+buffer[(*chars_written)++] = s[i];
+}
+}
+/**
+* print_int - prints an integer
+* @args: va_list of arguments
+* @buffer: buffer to write to
+* @chars_written: pointer to number of characters written
+*/
+void print_int(va_list args, char *buffer, int *chars_written)
+{
+int num = va_arg(args, int);
+char num_str[20];
+int i = 0;
+if (num < 0)
+{
+buffer[(*chars_written)++] = '-';
+num = -num;
+}
+if (num == 0)
+{
+num_str[i++] = '0';
+}
+while (num > 0)
+{
+num_str[i++] = num % 10 + '0';
+num /= 10;
+}
+while (i > 0)
+{
+buffer[(*chars_written)++] = num_str[--i];
+}
+}
+/**
+* _printf - custom printf function
+* @format: format string
+* Return: number of characters printed
+*/
 int _printf(const char *format, ...)
 {
 va_list args;
@@ -9,61 +70,27 @@ while (*format != '\0')
 {
 if (*format == '%')
 {
-format++;  /* move past the '%' */
+format++; /* move past the '%' */
 switch (*format)
 {
 case 'c':
-{
-char c = (char) va_arg(args, int);
-buffer[chars_written++] = c;
+print_char(args, buffer, &chars_written);
 break;
-}
 case 's':
-{
-char *s = va_arg(args, char *);
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-buffer[chars_written++] = s[i];
-}
+print_string(args, buffer, &chars_written);
 break;
-}
 case '%':
-{
 buffer[chars_written++] = '%';
 break;
-}
 case 'd':
-{
-    int num = va_arg(args, int);
-    char num_str[20];
-    int i = 0;
-    if (num < 0) {
-        buffer[chars_written++] = '-';
-        num = -num;
-    }
-    if (num == 0) {
-        num_str[i++] = '0';
-    }
-    while (num > 0) {
-        num_str[i++] = num % 10 + '0';
-        num /= 10;
-    }
-    while (i > 0) {
-        buffer[chars_written++] = num_str[--i];
-    }
+print_int(args, buffer, &chars_written);
 break;
-}
 default:
-{
 break;
-}
 }
 }
 else
-{
-buffer[chars_written++] = *format;
-}
+	buffer[chars_written++] = *format;
 if (chars_written == 1024)
 {
 write(1, buffer, chars_written);
@@ -72,9 +99,7 @@ chars_written = 0;
 format++;
 }
 if (chars_written > 0)
-write(1, buffer, chars_written);
+	write(1, buffer, chars_written);
 va_end(args);
-{
 return (chars_written);
-}
 }
