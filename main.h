f6a413b072f355bb373f4b0e039f3445d2d3864a
Feroza Chishty <chishty452@gmail.com>
2023-03-28 13:36:52 -0700

    testing task 0

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
-#ifndef MAINTEST_H
-#define MAINTEST_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
-int d_spec(int a);
+
 
 #endif
