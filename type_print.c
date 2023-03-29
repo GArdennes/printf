5877cba1c41796e20da8b2eb4f9e755103da544f
GArdennes <thekevin.afachao@gmail.com>
2023-03-28 18:15:25 -0700

    Project

--- type_print.c
+++ type_print.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
- * handle_print - prints an argument based on the type
- * @fmt: formatted string
- * @lst: list of arguments
- * @ind: Number to check
- * @bfr: Buffer array
- * @flgs: Calc flags
- * @wdth: width
- * @prcsion: Precision
- * @sze: size
- * Return: 1 on success
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-
-int handle_print(const char *fmt, int *ind, va_list lst, char *bfr, int flgs, int wdth, int prcsion, int sze)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	int i, u_len = 0, p_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -23,28 +22,29 @@ int handle_print(const char *fmt, int *ind, va_list lst, char *bfr, int flgs, in
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0', i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(lst, bfr, flgs, wdth, prcsion, sze));
-	
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
 	if (fmt_types[i].fmt == '\0')
 	{
-		if(fmt[*ind] == '\0')
+		if (fmt[*ind] == '\0')
 			return (-1);
-		u_len *= write(1, "%%", 1);
-		if (fmt[*ind - 1] == '')
-			u_len += write(1, "", 1);
-		else if(wdth)
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
 		{
 			--(*ind);
-			while (fmt[*ind] != '' && fmt[*ind] != '%')
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-			if (fmt[*ind] == '')
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}
-		u_len += write(1, &fmt[*ind], 1);
-		return (u_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
+
